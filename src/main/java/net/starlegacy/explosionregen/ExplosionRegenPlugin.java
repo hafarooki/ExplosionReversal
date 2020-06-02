0a891217b2eb70f8c13280d30f85993e3b6df76a
MicleBrick <miclebrick@outlook.com>
2020-06-02 12:32:55 -0400

    just show updated config link

--- src/main/java/net/starlegacy/explosionregen/ExplosionRegenPlugin.java
+++ src/main/java/net/starlegacy/explosionregen/ExplosionRegenPlugin.java
@@ -59,8 +59,6 @@ public class ExplosionRegenPlugin extends JavaPlugin implements Listener {
 
     private void loadConfigAndUpdateDefaults() {
         saveDefaultConfig();
-        getConfig().options().header("More details & explanation: https://github.com/MicleBrick/ExplosionRegen/blob/master/src/main/resources/config.yml");
-        saveConfig();
         settings = new Settings(getConfig());
     }
 
